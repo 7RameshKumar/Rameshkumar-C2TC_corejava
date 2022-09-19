05f44cf868dd0767ceadb24d79e2117be8a55886
Ramesh Kumar <113459605+7RameshKumar@users.noreply.github.com>
2022-09-19 11:56:51 +0530

    Create student.java

---
+++ Assignment-2/student.java
@@ -0,0 +1,14 @@
+package first_project;
+
+public class Student {
+	
+	  Student(){
+		System.out.println("The object of student  is created");
+	}
+
+	public static void main(String[] args) {
+		
+		Student xc=new Student();
+	}
+
+}
