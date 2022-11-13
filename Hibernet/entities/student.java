8d84449dace5cfca05c76cca438c30dcd40b0995
Ramesh Kumar <113459605+7RameshKumar@users.noreply.github.com>
2022-11-13 19:20:00 +0530

    Create student.java

---
+++ Hibernet/entities/student.java
@@ -0,0 +1,33 @@
+package com.tns.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Student implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private int studentid;
+	private String name;
+	
+	public int getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(int studentid) {
+		this.studentid = studentid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+Footer
+© 2022 GitHub, Inc.
